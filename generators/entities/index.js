6351f31cc1fd9d678cd811d99fe28af27970173c
Gianluca Troiani <gianluca.troiani@cnr.it>
2024-02-23 16:55:06 +0100

    pulizia codice, aggiunti commenti

--- generators/entities/index.js
+++ generators/entities/index.js
@@ -33,6 +33,7 @@ module.exports = class extends Generator {
         message: "Entities definition file path",
         default: '.vamp.entities.json'
       },{
+        store: true,
         type: "confirm",
         name: "migrateFresh",
         message: "Cleanup database?",
@@ -43,16 +44,21 @@ module.exports = class extends Generator {
   }
   
   writing() {
-    this.log(colors.dim(JSON.stringify(this.answers)));
-    if(!this.answers.build && !this.answers.rebuild) { return; }
+    if(!this.answers.build && !this.answers.rebuild) {
+      // Nothing to do
+      return;
+    }
     const entitiesFilePath = this.answers.entitiesFilePath[0] === '/' ? this.answers.entitiesFilePath : this.destinationPath(this.answers.entitiesFilePath);
     if(!this.fs.exists(entitiesFilePath)) {
+      // Entities definition file not found, nothing to do
       this.log(colors.red(`! Entities configuration file (${entitiesFilePath}) does not exists; no entities will be generated`));
-      return
+      return;
     } else {
       this.log(colors.green(`Entities configuration file found! Generating entities from ${entitiesFilePath}`));
     }
+    // Reading entities definition file
     const entities = this.fs.readJSON(entitiesFilePath) || {};
+    // Parsing entities from entities definition file
     if(Array.isArray(entities.entities)) {
       for (let index = 0; index < entities.entities.length; index++) {
         const entity = entities.entities[index];
@@ -62,8 +68,10 @@ module.exports = class extends Generator {
           ups.push(utils.getAddColumnUp(col, entity.schema[col]));
           downs.push(utils.getAddColumnDown(col));
         }
+        // Create entity table migration file
         this.spawnCommandSync('php', ['artisan', 'make:migration', `create_${utils.getTableNameFromEntityName(entity.name)}_table`], {cwd: 'server'});
         const migrationFilePath = `server/database/migrations/${moment().format("YYYY_MM_DD_HHmmss")}_add_columns_to_${utils.getTableNameFromEntityName(entity.name)}_table.php`;
+        // Create migration file for columns in entity table
         this.fs.copyTpl(this.templatePath("make_migrations_update_table.php.ejs"), this.destinationPath(migrationFilePath),
         {
           tabName: utils.getTableNameFromEntityName(entity.name),
@@ -72,9 +80,11 @@ module.exports = class extends Generator {
         });
       }
     }
+    // Parsing relations from entities definition file
     if(Array.isArray(entities.relations)) {
       const ups = [];
       const downs = [];
+      // Create migration files for relations
       for (let index = 0; index < entities.relations.length; index++) {
         const relation = entities.relations[index];
         ups.push(utils.getAddRelationUp(relation));
@@ -88,66 +98,10 @@ module.exports = class extends Generator {
         });
       }
     }
-    // if(!this.fs.exists(entitiesFilePath)) {
-    //   this.log(colors.red(`! Entities configuration file (${entitiesFilePath}) does not exists; no entities will be generated`))
-    // } else {
-    //   this.log(colors.green(`Entities configuration file found! Generating entities from ${entitiesFilePath}`));
-    // }
-    // const entities = this.fs.readJSON(entitiesFilePath) || [];
-    // const entitiesPascalNames = entities.map(e => to.pascal(e.name));
-    // entities.forEach(e => {
-    //   let mongooseSchema = Object.assign({}, e.schema);
-    //   Object.keys(mongooseSchema).map(function(key) {
-    //     mongooseSchema[key] = mongooseSchema[key] === "Datetime" ? "Date" : mongooseSchema[key];
-    //   });
-    //   const pascalName = to.pascal(e.name);
-    //   const columns = (Object.entries(e.schema)).map(e => ({
-    //     title: to.title(e[0]),
-    //     field: e[0],
-    //     type: e[1].toLowerCase().replace(/^number$/, "numeric")
-    //   }));
-    //   this.fs.copyTpl(
-    //       this.templatePath("server/entities/entity.model.js.ejs"),
-    //       this.destinationPath(`server/entities/${pascalName}/${pascalName}.model.js`),
-    //       {
-    //         entitySchemaName: pascalName,
-    //         entitySchema: JSON.stringify(mongooseSchema, null, 4)
-    //       }
-    //   );
-    //   this.fs.copyTpl(
-    //       this.templatePath("server/entities/entity.routes.js.ejs"),
-    //       this.destinationPath(`server/entities/${pascalName}/${pascalName}.routes.js`),
-    //       {
-    //         entitySchemaName: pascalName
-    //       }
-    //   );
-    //   this.fs.copyTpl(
-    //       this.templatePath("client/src/Entities/Items.js.ejs"),
-    //       this.destinationPath(`client/src/Entities/${pascalName}.js`),
-    //       {
-    //         componentName: pascalName,
-    //         componentTitle: to.title(pascalName),
-    //         componentApiPath: to.slug(pascalName),
-    //         componentColumns: columns
-    //       }
-    //   );
-    // });
-    // this.fs.copyTpl(this.templatePath("server/entities/entities.js.ejs"), this.destinationPath("server/entities/entities.js"), { to: to, entities: entitiesPascalNames});
-    // this.fs.copyTpl(this.templatePath("client/src/EntitiesRoutes.js.ejs"), this.destinationPath("client/src/EntitiesRoutes.js"), {
-    //   entities: entities.map(e => ({
-    //     path: to.slug(e.name),
-    //     componentName: to.pascal(e.name)
-    //   }))
-    // });
-    // this.fs.copyTpl(this.templatePath("client/src/EntitiesNavigation.js.ejs"), this.destinationPath("client/src/EntitiesNavigation.js"), {
-    //   entities: entities.map(e => ({
-    //     path: to.slug(e.name),
-    //     name: to.title(e.name)
-    //   }))
-    // });
   }
   end() {
     if(this.answers.build || this.answers.rebuild) {
+      // Execute migrations
       if(this.answers.migrateFresh) {
         this.spawnCommandSync('php', ['artisan', 'migrate:fresh'], {cwd: 'server'});
       } else {
