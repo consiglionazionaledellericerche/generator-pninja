205a0fdb38352b056f3c8a6fe28052b761dc3e26
Gianluca Troiani <gianluca.troiani@cnr.it>
2024-03-04 16:22:30 +0100

    modificata ASCII art

--- generators/app/utils.js
+++ generators/app/utils.js
@@ -2,11 +2,11 @@ const colors = require('ansi-colors');
 
 const hello = (log) => {
 log(colors.redBright(`\nIt's a Bird? It's a Plane? It's Superman? `) + colors.blueBright(`It's a `) + colors.bold.blueBright(`Rocket Elephant`) + colors.redBright('!\n'));
-log(colors.blueBright('    ____  ________  _____/ /_____ ') + `   ` + colors.redBright('  ▄▄▄'));
-log(colors.blueBright('   / __ \\/ ___/ _ \\/ ___/ __/ __ \\') + `   ` + colors.white('   ▄▄') + colors.blueBright('▄▄▄▄'));
-log(colors.blueBright('  / /_/ / /  /  __(__  ) /_/ /_/ /') + `   ` + colors.yellowBright('■■█') + colors.white('██') + colors.blueBright('███▄█'));
-log(colors.blueBright(' / .___/_/   \\___/____/\\__/\\____/ ') + `   ` + colors.white('   ▀▀') + colors.blueBright('▀▀▀▀█') + colors.whiteBright('▀'));
-log(colors.blueBright('/_/                               ') + `   ` + colors.redBright('  ▀▀▀') + colors.blueBright('   ▀'));
+log(colors.blueBright(`██████  ██████  ███████ ███████ ████████  ██████    `) + colors.redBright('  ▄▄▄'));
+log(colors.blueBright(`██   ██ ██   ██ ██      ██         ██    ██    ██   `) + colors.white('   ▄▄') + colors.blueBright('▄▄▄▄'));
+log(colors.blueBright(`██████  ██████  █████   ███████    ██    ██    ██   `) + colors.yellowBright('■■█') + colors.white('██') + colors.blueBright('███▄█'));
+log(colors.blueBright(`██      ██   ██ ██           ██    ██    ██    ██   `) + colors.white('   ▀▀') + colors.blueBright('▀▀▀▀█') + colors.whiteBright('▀'));
+log(colors.blueBright(`██      ██   ██ ███████ ███████    ██     ██████    `) + colors.redBright('  ▀▀▀') + colors.blueBright('   ▀'));
 log(`\n${colors.bold.blueBright(`Presto`)}: ${colors.bold.whiteBright(`P`)}${colors.white(`hp, `)}${colors.bold.whiteBright(`RE`)}${colors.white(`act, `)}${colors.bold.whiteBright(`S`)}${colors.white(`ql `)}${colors.bold.whiteBright(`TO`)}${colors.white(`olkit`)} ${colors.yellow(`v.0.1.0`)}`);
 }
 
