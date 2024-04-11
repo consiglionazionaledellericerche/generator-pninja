617cd1b764397374ff75aa672a56463daf817586
Gianluca Troiani <gianluca.troiani@cnr.it>
2024-04-11 18:43:15 +0200

    inclusione bootstrap

--- generators/client/templates/vue/vite.config.js
+++ generators/client/templates/vue/vite.config.js
@@ -9,9 +9,11 @@ export default defineConfig((mode) => {
     plugins: [vue()],
     server: {
       port: env.VITE_APP_PORT,
+      hot: true
     },
     resolve: {
       alias: {
+        '~bootstrap': fileURLToPath(new URL('./node_modules/bootstrap', import.meta.url)),
         '@': fileURLToPath(new URL('./src', import.meta.url)),
         '@services': fileURLToPath(new URL('./src/services', import.meta.url)),
         '@components': fileURLToPath(new URL('./src/components', import.meta.url))
