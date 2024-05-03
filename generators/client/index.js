2496994e5c419967f46b1d499506587dd6d1c1f8
Gianluca Troiani <gianluca.troiani@cnr.it>
2024-05-03 16:32:27 +0200

    fix per lanciare yo presto:client autonomamente e formattazione codice

--- generators/client/index.js
+++ generators/client/index.js
@@ -4,38 +4,38 @@ const fs = require('fs');
 const to = require('to-case');
 const colors = require('ansi-colors');
 const tab = '    ';
-const {createVueClient} = require('./vue.inc');
+const { createVueClient } = require('./vue.inc');
 module.exports = class extends Generator {
     constructor(args, opts) {
         super(args, opts);
     }
     async prompting() {
         let prompts = [];
-        if(this.options["fromMain"]) {
+        if (this.options["fromMain"]) {
             prompts = [...prompts, ...[{
                 store: true,
                 type: "list",
                 name: "clientType",
                 message: `Which ${colors.yellow('*Framework*')} would you like to use for the client?`,
                 choices: [
-                    {name: 'Vue', value: 'vue'},
-                    {name: 'React (Not implemented yet)', value: 'react'},
-                    {name: 'Angular (Not implemented yet)', value: 'angular'},
-                    {name: 'No client', value: false}
+                    { name: 'Vue', value: 'vue' },
+                    { name: 'React (Not implemented yet)', value: 'react' },
+                    { name: 'Angular (Not implemented yet)', value: 'angular' },
+                    { name: 'No client', value: false }
                 ]
             }]]
         }
         this.answers = await this.prompt(prompts);
     }
-    
+
     configuring() {
-        for(const key in this.answers){
+        for (const key in this.answers) {
             this.config.set(key, this.answers[key]);
         }
         this.config.save();
     }
-    
+
     async writing() {
-        if(this.answers.clientType === 'vue') await createVueClient(this);
+        if (this.config.get('clientType') === 'vue') await createVueClient(this);
     }
 };
