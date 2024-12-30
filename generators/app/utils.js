3cc57e997ef818df9701cc1e1da53e65e20ee1d2
Gianluca Troiani <gianluca.troiani@cnr.it>
2024-12-30 21:16:12 +0100

    cambiato banner

--- generators/app/utils.js
+++ generators/app/utils.js
@@ -2,12 +2,24 @@ import colors from 'ansi-colors';
 
 class Utils {
     hello(log) {
-        log(colors.redBright(`\nIt's a Bird? It's a Plane? It's Superman? `) + colors.blueBright(`It's a `) + colors.bold.blueBright(`Rocket Elephant`) + colors.redBright('!\n'));
-        log(colors.blue(` ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ `) + colors.redBright('  ▄▄▄'));
-        log(colors.blueBright(` ▄▀▀█ ▄▀▀█ ▄▀▀ ▄▀▀ ▀█▀ ▄▀▀█ `) + colors.white('   ▄▄') + colors.blueBright('▄▄▄▄'));
-        log(colors.blueBright(` █▄▄▀ █▄▄▀ █▀▀ ▀▀█  █  █  █ `) + colors.yellowBright('■■█') + colors.white('██') + colors.blueBright('███▄█'));
-        log(colors.blueBright(` █    ▀ ▀▀ ▀▀▀ ▀▀   ▀  ▀▀▀  `) + colors.white('   ▀▀') + colors.blueBright('▀▀▀▀█') + colors.whiteBright('▀'));
-        log(colors.blue(` ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ `) + colors.redBright('  ▀▀▀') + colors.blueBright('   ▀'));
+        //    ###############                                                 
+        //     ###############                                                
+        //      ############### ########  ########  ######  ########  ####### 
+        // =======     ######## ##     ## ##       ##    ##    ##    ##     ##
+        //  =====     ########  ##     ## ##       ##          ##    ##     ##
+        //   ===     ########   ########  ######    ######     ##    ##     ##
+        //    ========          ##   ##   ##             ##    ##    ##     ##
+        //     ========         ##    ##  ##       ##    ##    ##    ##     ##
+        //      ========        ##     ## ########  ######     ##     ####### 
+        log(colors.cyan('   ###############                                                 '));
+        log(colors.cyan('    ###############                                                '));
+        log(colors.cyan('     ############### ########  ########  ######  ########  ####### '));
+        log(colors.cyanBright('=======') + colors.cyan('     ######## ##     ## ##       ##    ##    ##    ##     ##'));
+        log(colors.cyanBright(' =====') + colors.cyan('     ########  ##     ## ##       ##          ##    ##     ##'));
+        log(colors.cyanBright('  ===') + colors.cyan('     ########   ########  ######    ######     ##    ##     ##'));
+        log(colors.cyanBright('   ========') + colors.cyan('          ##   ##   ##             ##    ##    ##     ##'));
+        log(colors.cyanBright('    ========') + colors.cyan('         ##    ##  ##       ##    ##    ##    ##     ##'));
+        log(colors.cyanBright('     ========') + colors.cyan('        ##     ## ########  ######     ##     ####### '));
         log(`\n${colors.bold.blueBright(`Presto`)}: ${colors.bold.whiteBright(`P`)}${colors.white(`hp, `)}${colors.bold.whiteBright(`RE`)}${colors.white(`act, `)}${colors.bold.whiteBright(`S`)}${colors.white(`ql `)}${colors.bold.whiteBright(`TO`)}${colors.white(`olkit`)} ${colors.yellow(`v.0.2.0`)}`);
     }
 }
