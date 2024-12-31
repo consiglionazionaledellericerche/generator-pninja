33225146fc07e5de5d9b0e3bafb28f84f5658db1
Gianluca Troiani <gianluca.troiani@gmail.com>
2024-12-31 18:53:43 +0100

    banner monocromatico per meglio sfruttare ASCII art

--- generators/app/utils.js
+++ generators/app/utils.js
@@ -11,15 +11,15 @@ class Utils {
         //    ========          ##   ##   ##             ##    ##    ##     ##
         //     ========         ##    ##  ##       ##    ##    ##    ##     ##
         //      ========        ##     ## ########  ######     ##     ####### 
-        log(colors.cyan('   ###############                                                 '));
-        log(colors.cyan('    ###############                                                '));
-        log(colors.cyan('     ############### ########  ########  ######  ########  ####### '));
-        log(colors.cyanBright('=======') + colors.cyan('     ######## ##     ## ##       ##    ##    ##    ##     ##'));
-        log(colors.cyanBright(' =====') + colors.cyan('     ########  ##     ## ##       ##          ##    ##     ##'));
-        log(colors.cyanBright('  ===') + colors.cyan('     ########   ########  ######    ######     ##    ##     ##'));
-        log(colors.cyanBright('   ========') + colors.cyan('          ##   ##   ##             ##    ##    ##     ##'));
-        log(colors.cyanBright('    ========') + colors.cyan('         ##    ##  ##       ##    ##    ##    ##     ##'));
-        log(colors.cyanBright('     ========') + colors.cyan('        ##     ## ########  ######     ##     ####### '));
+        log(colors.cyanBright('   ###############                                                 '));
+        log(colors.cyanBright('    ###############                                                '));
+        log(colors.cyanBright('     ############### ########  ########  ######  ########  ####### '));
+        log(colors.cyanBright('=======     ######## ##     ## ##       ##    ##    ##    ##     ##'));
+        log(colors.cyanBright(' =====     ########  ##     ## ##       ##          ##    ##     ##'));
+        log(colors.cyanBright('  ===     ########   ########  ######    ######     ##    ##     ##'));
+        log(colors.cyanBright('   ========          ##   ##   ##             ##    ##    ##     ##'));
+        log(colors.cyanBright('    ========         ##    ##  ##       ##    ##    ##    ##     ##'));
+        log(colors.cyanBright('     ========        ##     ## ########  ######     ##     ####### '));
         log(`\n${colors.bold.blueBright(`Presto`)}: ${colors.bold.whiteBright(`P`)}${colors.white(`hp, `)}${colors.bold.whiteBright(`RE`)}${colors.white(`act, `)}${colors.bold.whiteBright(`S`)}${colors.white(`ql `)}${colors.bold.whiteBright(`TO`)}${colors.white(`olkit`)} ${colors.yellow(`v.0.2.0`)}`);
     }
 }
