2f362b627902c9da84ce715630f1392e9db3ab4a
Gianluca Troiani <gianluca.troiani@cnr.it>
2025-01-07 15:46:09 +0100

    aggiornato banner

--- generators/app/utils.js
+++ generators/app/utils.js
@@ -2,25 +2,15 @@ import colors from 'ansi-colors';
 
 class Utils {
     hello(log) {
-        //    ###############                                                 
-        //     ###############                                                
-        //      ############### ########  ########  ######  ########  ####### 
-        // =======     ######## ##     ## ##       ##    ##    ##    ##     ##
-        //  =====     ########  ##     ## ##       ##          ##    ##     ##
-        //   ===     ########   ########  ######    ######     ##    ##     ##
-        //    ========          ##   ##   ##             ##    ##    ##     ##
-        //     ========         ##    ##  ##       ##    ##    ##    ##     ##
-        //      ========        ##     ## ########  ######     ##     ####### 
-        log(colors.cyanBright('   ###############                                                 '));
-        log(colors.cyanBright('    ###############                                                '));
-        log(colors.cyanBright('     ############### ########  ########  ######  ########  ####### '));
-        log(colors.cyanBright('=======     ######## ##     ## ##       ##    ##    ##    ##     ##'));
-        log(colors.cyanBright(' =====     ########  ##     ## ##       ##          ##    ##     ##'));
-        log(colors.cyanBright('  ===     ########   ########  ######    ######     ##    ##     ##'));
-        log(colors.cyanBright('   ========          ##   ##   ##             ##    ##    ##     ##'));
-        log(colors.cyanBright('    ========         ##    ##  ##       ##    ##    ##    ##     ##'));
-        log(colors.cyanBright('     ========        ##     ## ########  ######     ##     ####### '));
-        log(`\n${colors.bold.blueBright(`Presto`)}: ${colors.bold.whiteBright(`P`)}${colors.white(`hp, `)}${colors.bold.whiteBright(`RE`)}${colors.white(`act, `)}${colors.bold.whiteBright(`S`)}${colors.white(`ql `)}${colors.bold.whiteBright(`TO`)}${colors.white(`olkit`)} ${colors.yellow(`v.0.2.0`)}`);
+        log(colors.magenta('████████████ ████████████ ████             ████████ ████████████ ████████████'));
+        log(colors.magenta('████████████ ████████████ ████             ████████ ████████████ ████████████'));
+        log(colors.magenta('████    ████ ████         ████████         ████         ████     ████    ████'));
+        log(colors.magenta('████    ████ ████         ████████         ████         ████     ████    ████'));
+        log(colors.magenta('    ████████ ████             ████████ ████████         ████     ████████████'));
+        log(colors.magenta('    ████████ ████             ████████ ████████         ████     ████████████'));
+        log(colors.magenta('████                                                                         '));
+        log(colors.magenta('████                                                                         '));
+        log(`\n${colors.bold.magenta(`Presto`)}: ${colors.bold.magenta(`P`)}${colors.white(`hp, `)}${colors.bold.magenta(`RE`)}${colors.white(`act, `)}${colors.bold.magenta(`S`)}${colors.white(`ql `)}${colors.bold.magenta(`TO`)}${colors.white(`olkit`)} ${colors.yellow(`v.0.2.0`)}`);
     }
 }
 
