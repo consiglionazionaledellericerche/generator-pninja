18cdb71b385a23b4ac31a68e6286c7a924362c2e
Gianluca Troiani <gianluca.troiani@cnr.it>
2025-01-31 12:28:41 +0100

    generazione con entità correlate da relazioni dirette

--- generators/entities/utils/controllers-generator.js
+++ generators/entities/utils/controllers-generator.js
@@ -3,6 +3,7 @@ import pluralize from 'pluralize';
 import jhipsterCore from 'jhipster-core';
 import jclrz from 'json-colorz';
 const { parseFromFiles } = jhipsterCore;
+import _ from 'lodash';
 
 export class ControllersGenerator {
     constructor(that, entitiesFilePath) {
@@ -16,6 +17,7 @@ export class ControllersGenerator {
         const { enums, entities, relationships } = this.parsedJDL;
         for (const entity of entities) {
             const withs = [];
+            const createRelated = [];
             relationships.forEach(relation => {
                 if (!relation.from.injectedField && !relation.to.injectedField) {
                     relation.to.injectedField = relation.from.name;
@@ -29,7 +31,46 @@ export class ControllersGenerator {
                 (relation.cardinality === 'OneToOne' || relation.cardinality === 'OneToMany' || relation.cardinality === 'ManyToMany') && relation.from.name === entity.name
                 && (!!relation.from.injectedField || (!relation.from.injectedField && !relation.to.injectedField))
             )).forEach(relation => {
-                withs.push(`'${to.snake(relation.from.injectedField || relation.to.name)}'`);
+                const fromField = to.snake(relation.from.injectedField || relation.to.name);
+                const toEntity = relation.to.name;
+                withs.push(`'${fromField}'`);
+                if (relation.cardinality === 'OneToOne') {
+                    createRelated.push(`
+        if(array_key_exists("${fromField}", $request->all()) && $request->all()["${fromField}"]) {
+            $request_${fromField} = $request->all()["${fromField}"];
+            if (is_numeric($request_${fromField})) {
+                $${fromField} = \\App\\Models\\${toEntity}::findOrFail($request_${fromField});
+            } elseif (array_key_exists("id", $request_${fromField})) {
+                $${fromField} = \\App\\Models\\${toEntity}::findOrFail($request_${fromField}["id"]);
+                $${fromField}->update($request_${fromField});
+            } else {
+                $${fromField} = new \\App\\Models\\${toEntity}($request_${fromField});
+            }
+            $${to.camel(entity.name)}->${fromField}()->save($${fromField});
+        };`);
+                }
+                if (relation.cardinality === 'OneToMany') {
+                    createRelated.push(`
+        if(array_key_exists("${fromField}", $request->all()) && $request->all()["${fromField}"]) {
+            $related = array_map(function($o) {
+                if(is_numeric($o)) return \\App\\Models\\${toEntity}::findOrFail($o);
+                if(array_key_exists("id", $o)) return \\App\\Models\\${toEntity}::findOrFail($o["id"]);
+                return new \\App\\Models\\${toEntity}($o);
+            }, $request->all()["${fromField}"]);
+            $${to.camel(entity.name)}->${fromField}()->saveMany($related);
+        };`);
+                }
+                if (relation.cardinality === 'ManyToMany') {
+                    createRelated.push(`
+        if(array_key_exists("${fromField}", $request->all()) && $request->all()["${fromField}"]) {
+            $ids = array_map(function($o) {
+                if(is_numeric($o)) return $o;
+                if(array_key_exists("id", $o)) return (\\App\\Models\\${toEntity}::findOrFail($o["id"]))->id;
+                return (\\App\\Models\\${toEntity}::create($o))->id;
+            }, $request->all()["${fromField}"]);
+            $${to.camel(entity.name)}->${fromField}()->sync($ids);
+        };`);
+                }
             });
 
             // OneToOne/OneToMany/ManyToMany reverse relationships
@@ -45,7 +86,22 @@ export class ControllersGenerator {
                 relation.cardinality === 'ManyToOne' && relation.from.name === entity.name
                 && (!!relation.from.injectedField || (!relation.from.injectedField && !relation.to.injectedField))
             )).forEach(relation => {
-                withs.push(`'${to.snake(relation.from.injectedField)}'`);
+                const fromField = to.snake(relation.from.injectedField || relation.to.name);
+                const toEntity = relation.to.name;
+                withs.push(`'${fromField}'`);
+                createRelated.push(`
+		if(array_key_exists("${fromField}", $request->all()) && $request->all()["${fromField}"]) {
+            $request_country = $request->all()["${fromField}"];
+            if (is_numeric($request_country)) {
+                $${fromField} = \\App\\Models\\${toEntity}::findOrFail($request_country);
+            } elseif (array_key_exists("id", $request_country)) {
+                $${fromField} = \\App\\Models\\${toEntity}::findOrFail($request_country["id"]);
+            } else {
+                $${fromField} = \\App\\Models\\${toEntity}::create($request_country);
+            }
+            $${to.camel(entity.name)}->${fromField}()->associate($${fromField});
+            $${to.camel(entity.name)}->save();
+        };`);
             });
 
             // ManyToOne reverse relationships
@@ -61,7 +117,8 @@ export class ControllersGenerator {
                     className: entity.name,
                     entityName: to.camel(entity.name),
                     withs: withs.length ? `[${withs.join(', ')}]` : null, // (_.compact([...withs, ...inverseWiths]).length) ? `['${_.compact([...withs, ...inverseWiths]).join(`','`)}']` : null,
-                    createRelated: [], //_.compact([...createRelated, ...createInverseRelated]).join("\n\n\t\t")
+                    createRelated: _.compact(createRelated).join("\n\n\t\t")
+                    // createRelated: [], //_.compact([...createRelated, ...createInverseRelated]).join("\n\n\t\t")
                 });
         }
     }
