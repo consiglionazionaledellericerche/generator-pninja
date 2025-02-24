82567fdb6e1c0db449f0b94d450e59b97a5058c1
Gianluca Troiani <gianluca.troiani@cnr.it>
2025-02-24 18:46:12 +0100

    migliorata formattazione codice generato per EntityController.php; rimoninato template per maggiore chiarezza

--- generators/entities/utils/controllers-generator.js
+++ generators/entities/utils/controllers-generator.js
@@ -42,40 +42,40 @@ export class ControllersGenerator {
                 withs.push(`'${fromField}'`);
                 if (relation.cardinality === 'OneToOne') {
                     createRelated.push(`
-        if(array_key_exists("${fromField}", $request->all()) && $request->all()["${fromField}"]) {
-            $request_${fromField} = $request->all()["${fromField}"];
-            if (is_numeric($request_${fromField})) {
-                $${fromField} = \\App\\Models\\${toEntity}::findOrFail($request_${fromField});
-            } elseif (array_key_exists("id", $request_${fromField})) {
-                $${fromField} = \\App\\Models\\${toEntity}::findOrFail($request_${fromField}["id"]);
-                $${fromField}->update($request_${fromField});
-            } else {
-                $${fromField} = new \\App\\Models\\${toEntity}($request_${fromField});
-            }
-            $${to.camel(entity.name)}->${fromField}()->save($${fromField});
-        };`);
+            if(array_key_exists("${fromField}", $request->all()) && $request->all()["${fromField}"]) {
+                $request_${fromField} = $request->all()["${fromField}"];
+                if (is_numeric($request_${fromField})) {
+                    $${fromField} = \\App\\Models\\${toEntity}::findOrFail($request_${fromField});
+                } elseif (array_key_exists("id", $request_${fromField})) {
+                    $${fromField} = \\App\\Models\\${toEntity}::findOrFail($request_${fromField}["id"]);
+                    $${fromField}->update($request_${fromField});
+                } else {
+                    $${fromField} = new \\App\\Models\\${toEntity}($request_${fromField});
+                }
+                $${to.camel(entity.name)}->${fromField}()->save($${fromField});
+            };`);
                 }
                 if (relation.cardinality === 'OneToMany') {
                     createRelated.push(`
-        if(array_key_exists("${fromField}", $request->all()) && $request->all()["${fromField}"]) {
-            $related = array_map(function($o) {
-                if(is_numeric($o)) return \\App\\Models\\${toEntity}::findOrFail($o);
-                if(array_key_exists("id", $o)) return \\App\\Models\\${toEntity}::findOrFail($o["id"]);
-                return new \\App\\Models\\${toEntity}($o);
-            }, $request->all()["${fromField}"]);
-            $${to.camel(entity.name)}->${fromField}()->saveMany($related);
-        };`);
+            if(array_key_exists("${fromField}", $request->all()) && $request->all()["${fromField}"]) {
+                $related = array_map(function($o) {
+                    if(is_numeric($o)) return \\App\\Models\\${toEntity}::findOrFail($o);
+                    if(array_key_exists("id", $o)) return \\App\\Models\\${toEntity}::findOrFail($o["id"]);
+                    return new \\App\\Models\\${toEntity}($o);
+                }, $request->all()["${fromField}"]);
+                $${to.camel(entity.name)}->${fromField}()->saveMany($related);
+            };`);
                 }
                 if (relation.cardinality === 'ManyToMany') {
                     createRelated.push(`
-        if(array_key_exists("${fromField}", $request->all()) && $request->all()["${fromField}"]) {
-            $ids = array_map(function($o) {
-                if(is_numeric($o)) return $o;
-                if(array_key_exists("id", $o)) return (\\App\\Models\\${toEntity}::findOrFail($o["id"]))->id;
-                return (\\App\\Models\\${toEntity}::create($o))->id;
-            }, $request->all()["${fromField}"]);
-            $${to.camel(entity.name)}->${fromField}()->sync($ids);
-        };`);
+            if(array_key_exists("${fromField}", $request->all()) && $request->all()["${fromField}"]) {
+                $ids = array_map(function($o) {
+                    if(is_numeric($o)) return $o;
+                    if(array_key_exists("id", $o)) return (\\App\\Models\\${toEntity}::findOrFail($o["id"]))->id;
+                    return (\\App\\Models\\${toEntity}::create($o))->id;
+                }, $request->all()["${fromField}"]);
+                $${to.camel(entity.name)}->${fromField}()->sync($ids);
+            };`);
                 }
             });
 
@@ -89,44 +89,44 @@ export class ControllersGenerator {
                 withs.push(`'${toField}'`);
                 if (relation.cardinality === 'OneToOne') {
                     createRelated.push(`
-        if(array_key_exists("${toField}", $request->all()) && $request->all()["${toField}"]) {
-            $request_${toField} = $request->all()["${toField}"];
-            if (is_numeric($request_${toField})) {
-                $${toField} = \\App\\Models\\${fromEntity}::findOrFail($request_${toField});
-            } elseif (array_key_exists("id", $request_${toField})) {
-                $${toField} = \\App\\Models\\${fromEntity}::findOrFail($request_${toField}["id"]);
-            } else {
-                $${toField} = \\App\\Models\\${fromEntity}::create($request_${toField});
-            }
-            $${to.camel(entity.name)}->${toField}()->associate($${toField});
-            $${to.camel(entity.name)}->save();
-        };`);
+            if(array_key_exists("${toField}", $request->all()) && $request->all()["${toField}"]) {
+                $request_${toField} = $request->all()["${toField}"];
+                if (is_numeric($request_${toField})) {
+                    $${toField} = \\App\\Models\\${fromEntity}::findOrFail($request_${toField});
+                } elseif (array_key_exists("id", $request_${toField})) {
+                    $${toField} = \\App\\Models\\${fromEntity}::findOrFail($request_${toField}["id"]);
+                } else {
+                    $${toField} = \\App\\Models\\${fromEntity}::create($request_${toField});
+                }
+                $${to.camel(entity.name)}->${toField}()->associate($${toField});
+                $${to.camel(entity.name)}->save();
+            };`);
                 }
                 if (relation.cardinality === 'OneToMany') {
                     createRelated.push(`
-        if(array_key_exists("${toField}", $request->all()) && $request->all()["${toField}"]) {
-            $request_${toField} = $request->all()["${toField}"];
-            if (is_numeric($request_${toField})) {
-                $${toField} = \\App\\Models\\${fromEntity}::findOrFail($request_${toField});
-            } elseif (array_key_exists("id", $request_${toField})) {
-                $${toField} = \\App\\Models\\${fromEntity}::findOrFail($request_${toField}["id"]);
-            } else {
-                $${toField} = \\App\\Models\\${fromEntity}::create($request_${toField});
-            }
-            $${to.camel(entity.name)}->${toField}()->associate($${toField});
-            $${to.camel(entity.name)}->save();
-        }`);
+            if(array_key_exists("${toField}", $request->all()) && $request->all()["${toField}"]) {
+                $request_${toField} = $request->all()["${toField}"];
+                if (is_numeric($request_${toField})) {
+                    $${toField} = \\App\\Models\\${fromEntity}::findOrFail($request_${toField});
+                } elseif (array_key_exists("id", $request_${toField})) {
+                    $${toField} = \\App\\Models\\${fromEntity}::findOrFail($request_${toField}["id"]);
+                } else {
+                    $${toField} = \\App\\Models\\${fromEntity}::create($request_${toField});
+                }
+                $${to.camel(entity.name)}->${toField}()->associate($${toField});
+                $${to.camel(entity.name)}->save();
+            }`);
                 }
                 if (relation.cardinality === 'ManyToMany') {
                     createRelated.push(`
-        if(array_key_exists("${toField}", $request->all()) && $request->all()["${toField}"]) {
-            $related = array_map(function($o) {
-                if(is_numeric($o)) return \\App\\Models\\${fromEntity}::findOrFail($o);
-                if(array_key_exists("id", $o)) return \\App\\Models\\${fromEntity}::findOrFail($o["id"]);
-                return new \\App\\Models\\${fromEntity}($o);
-            }, $request->all()["${toField}"]);
-            $${to.camel(entity.name)}->${toField}()->saveMany($related);
-        };`);
+            if(array_key_exists("${toField}", $request->all()) && $request->all()["${toField}"]) {
+                $related = array_map(function($o) {
+                    if(is_numeric($o)) return \\App\\Models\\${fromEntity}::findOrFail($o);
+                    if(array_key_exists("id", $o)) return \\App\\Models\\${fromEntity}::findOrFail($o["id"]);
+                    return new \\App\\Models\\${fromEntity}($o);
+                }, $request->all()["${toField}"]);
+                $${to.camel(entity.name)}->${toField}()->saveMany($related);
+            };`);
                 }
             });
 
@@ -139,18 +139,18 @@ export class ControllersGenerator {
                 const toEntity = relation.to.name;
                 withs.push(`'${fromField}'`);
                 createRelated.push(`
-		if(array_key_exists("${fromField}", $request->all()) && $request->all()["${fromField}"]) {
-            $request_${fromField} = $request->all()["${fromField}"];
-            if (is_numeric($request_${fromField})) {
-                $${fromField} = \\App\\Models\\${toEntity}::findOrFail($request_${fromField});
-            } elseif (array_key_exists("id", $request_${fromField})) {
-                $${fromField} = \\App\\Models\\${toEntity}::findOrFail($request_${fromField}["id"]);
-            } else {
-                $${fromField} = \\App\\Models\\${toEntity}::create($request_${fromField});
-            }
-            $${to.camel(entity.name)}->${fromField}()->associate($${fromField});
-            $${to.camel(entity.name)}->save();
-        };`);
+            if(array_key_exists("${fromField}", $request->all()) && $request->all()["${fromField}"]) {
+                $request_${fromField} = $request->all()["${fromField}"];
+                if (is_numeric($request_${fromField})) {
+                    $${fromField} = \\App\\Models\\${toEntity}::findOrFail($request_${fromField});
+                } elseif (array_key_exists("id", $request_${fromField})) {
+                    $${fromField} = \\App\\Models\\${toEntity}::findOrFail($request_${fromField}["id"]);
+                } else {
+                    $${fromField} = \\App\\Models\\${toEntity}::create($request_${fromField});
+                }
+                $${to.camel(entity.name)}->${fromField}()->associate($${fromField});
+                $${to.camel(entity.name)}->save();
+            };`);
             });
 
             // ManyToOne reverse relationships
@@ -162,17 +162,17 @@ export class ControllersGenerator {
                 const fromEntity = relation.from.name;
                 withs.push(`'${toField}'`);
                 createRelated.push(`
-        if(array_key_exists("${toField}", $request->all()) && $request->all()["${toField}"]) {
-            $related = array_map(function($o) {
-                if(is_numeric($o)) return \\App\\Models\\${fromEntity}::findOrFail($o);
-                if(array_key_exists("id", $o)) return \\App\\Models\\${fromEntity}::findOrFail($o["id"]);
-                return new \\App\\Models\\${fromEntity}($o);
-            }, $request->all()["${toField}"]);
-            $${to.camel(entity.name)}->${toField}()->saveMany($related);
-        };`);
+            if(array_key_exists("${toField}", $request->all()) && $request->all()["${toField}"]) {
+                $related = array_map(function($o) {
+                    if(is_numeric($o)) return \\App\\Models\\${fromEntity}::findOrFail($o);
+                    if(array_key_exists("id", $o)) return \\App\\Models\\${fromEntity}::findOrFail($o["id"]);
+                    return new \\App\\Models\\${fromEntity}($o);
+                }, $request->all()["${toField}"]);
+                $${to.camel(entity.name)}->${toField}()->saveMany($related);
+            };`);
             });
 
-            this.that.fs.copyTpl(this.that.templatePath("entity_controller.php.ejs"), this.that.destinationPath(`server/app/Http/Controllers/${entity.name}Controller.php`),
+            this.that.fs.copyTpl(this.that.templatePath("EntityController.php.ejs"), this.that.destinationPath(`server/app/Http/Controllers/${entity.name}Controller.php`),
                 {
                     className: entity.name,
                     entityName: to.camel(entity.name),
