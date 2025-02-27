b8fdb16fa487713d7cad5bc8f2d0483f3874fd64
Gianluca Troiani <gianluca.troiani@cnr.it>
2025-02-27 19:34:16 +0100

    Aggiunto Suspense al caricamento dell'applicazione

--- generators/client/templates/react/src/main.tsx
+++ generators/client/templates/react/src/main.tsx
@@ -1,7 +1,8 @@
-import { StrictMode } from 'react';
+import { StrictMode, Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ReactKeycloakProvider } from '@react-keycloak/web';
+import { SimpleLoader } from './components/SimpleLoader.tsx';
 import keycloak from './keycloak.tsx';
 import './index.css';
 import App from './App.tsx';
@@ -12,12 +13,20 @@ createRoot(document.getElementById('root')!).render(
     authClient={keycloak}
     initOptions={{
       // onLoad: 'check-sso', // 'login-required'/
-      checkLoginIframe: false,
+      checkLoginIframe: false
     }}
   >
     <StrictMode>
       <BrowserRouter>
-        <App />
+        <Suspense
+          fallback={
+            <div className="w-[100vw] h-[100vh] flex items-center justify-center">
+              <SimpleLoader loading={true} />
+            </div>
+          }
+        >
+          <App />
+        </Suspense>
       </BrowserRouter>
     </StrictMode>
   </ReactKeycloakProvider>
