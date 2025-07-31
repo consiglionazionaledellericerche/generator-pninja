5fb8925d524e29a55c0bfbf48d72016e98c48e8c
Gianluca Troiani <gianluca.troiani@cnr.it>
2025-07-31 20:19:15 +0200

    pulizia codice

--- generators/client/react.inc.js
+++ generators/client/react.inc.js
@@ -16,8 +16,8 @@ export async function createReactClient(that, parsedJDL) {
     that.fs.copyTpl(that.templatePath('react/index.html.ejs'), that.destinationPath('client/index.html'), { appName });
     that.fs.copyTpl(that.templatePath('react/package.json.ejs'), that.destinationPath('client/package.json'), { name: to.slug(appName) });
     that.fs.copyTpl(that.templatePath('react/package-lock.json.ejs'), that.destinationPath('client/package-lock.json'), { name: to.slug(appName) });
-    that.fs.copyTpl(that.templatePath('react/tsconfig.app.json'), that.destinationPath('client/tsconfig.app.json'), {});
-    that.fs.copyTpl(that.templatePath('react/tsconfig.json'), that.destinationPath('client/tsconfig.json'), {});
+    that.fs.copyTpl(that.templatePath('react/tsconfig.app.json.ejs'), that.destinationPath('client/tsconfig.app.json'), {});
+    that.fs.copyTpl(that.templatePath('react/tsconfig.json.ejs'), that.destinationPath('client/tsconfig.json'), {});
     that.fs.copyTpl(that.templatePath('react/tsconfig.node.json'), that.destinationPath('client/tsconfig.node.json'), {});
     that.fs.copyTpl(that.templatePath('react/vite.config.ts.ejs'), that.destinationPath('client/vite.config.ts'), {});
 
