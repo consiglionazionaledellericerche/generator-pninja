de99df291bb3a853f9a9d8add157643c14d7768c
Gianluca Troiani <gianluca.troiani@cnr.it>
2025-08-05 11:42:46 +0200

    implementato RadioGroup; related #15

--- generators/client/react.inc.js
+++ generators/client/react.inc.js
@@ -60,6 +60,7 @@ export async function createReactClient(that, parsedJDL) {
     that.fs.copyTpl(that.templatePath("react/src/components/forms/FormLabel.tsx.ejs"), that.destinationPath(`client/src/components/forms/FormLabel.tsx`), {});
     that.fs.copyTpl(that.templatePath("react/src/components/forms/InfoValidations.tsx.ejs"), that.destinationPath(`client/src/components/forms/InfoValidations.tsx`), {});
     that.fs.copyTpl(that.templatePath("react/src/components/forms/NumberField.tsx.ejs"), that.destinationPath(`client/src/components/forms/NumberField.tsx`), {});
+    that.fs.copyTpl(that.templatePath("react/src/components/forms/RadioGroup.tsx.ejs"), that.destinationPath(`client/src/components/forms/RadioGroup.tsx`), {});
     that.fs.copyTpl(that.templatePath("react/src/components/forms/SelectField.tsx.ejs"), that.destinationPath(`client/src/components/forms/SelectField.tsx`), {});
     that.fs.copyTpl(that.templatePath("react/src/components/forms/Textarea.tsx.ejs"), that.destinationPath(`client/src/components/forms/Textarea.tsx`), {});
     that.fs.copyTpl(that.templatePath("react/src/components/forms/TextField.tsx.ejs"), that.destinationPath(`client/src/components/forms/TextField.tsx`), {});
