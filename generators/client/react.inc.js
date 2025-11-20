eb56e18de4fbe3c71f004c7fde14880c37c28bc5
Gianluca Troiani <gianluca.troiani@cnr.it>
2025-11-20 11:17:31 +0100

    termini relativi a entità in file separato da termini "common"; related #45

--- generators/client/react.inc.js
+++ generators/client/react.inc.js
@@ -43,7 +43,8 @@ export async function createReactClient(that, parsedJDL) {
     that.fs.copyTpl(that.templatePath('react/vite.config.ts.ejs'), that.destinationPath('client/vite.config.ts'), {});
 
     for (const lang of languages) {
-        that.fs.copyTpl(that.templatePath(`react/public/locales/${lang}.json.ejs`), that.destinationPath(`client/public/locales/${lang}.json`), { appName, entities, relationships, to, pluralize, getModelForeignIds, getModelRelatedEntities });
+        that.fs.copyTpl(that.templatePath(`react/public/locales/${lang}.json.ejs`), that.destinationPath(`client/public/locales/${lang}/common.json`), { appName });
+        that.fs.copyTpl(that.templatePath(`react/public/locales/entities.json.ejs`), that.destinationPath(`client/public/locales/${lang}/entities.json`), { entities, relationships, to, pluralize, getModelForeignIds, getModelRelatedEntities });
     };
     that.fs.copyTpl(that.templatePath("react/public/fonts/IBMPlexMono-Regular.ttf"), that.destinationPath(`client/public/fonts/IBMPlexMono-Regular.ttf`));
     that.fs.copyTpl(that.templatePath("react/public/fonts/InterVariable-Italic.woff2"), that.destinationPath(`client/public/fonts/InterVariable-Italic.woff2`));
