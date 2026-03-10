99d18e2483127487b55215ff9e4fec98b07af200
Gianluca Troiani <gianluca.troiani@cnr.it>
2026-03-10 16:11:51 +0100

    Modifiche path in App.tsx e link nel menu e nella navigazione; related #72

--- generators/utils/AcRule.js
+++ generators/utils/AcRule.js
@@ -1,7 +1,7 @@
 export const AcRule = {
     "name": "AcRule",
     "tableName": "ac_rules",
-    "softDelete": false,
+    "softDelete": true,
     "pessimisticLock": true,
     "body": [
         {
